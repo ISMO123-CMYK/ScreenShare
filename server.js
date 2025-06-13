a52a5aa7f6081159b4732ecb6ef54575100d056d
Ismail Gidado <gidadoismail24@gmail.com>
2025-06-13 18:54:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const socketIo = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, { cors: { origin: '*' } });
+const fetch = require('node-fetch'); // v2 if using CommonJS
+const domain = 'https://screenshare-am2q.onrender.com/'; // change to your target domain
+
+const ping = async () => {
+  try {
+    const res = await fetch(domain);
+    console.log(`[${new Date().toISOString()}] Status: ${res.status}`);
+  } catch (err) {
+    console.error(`[${new Date().toISOString()}] Error:`, err.message);
+  }
+};
+
+setInterval(ping, 10000); // every 10 seconds
 app.use(express.static("public"))
 io.on('connection', socket => {
   console.log('User connected');
